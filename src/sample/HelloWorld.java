434a3ad04179fe1e5a99e717b33abe48d4edba33
Khasanboi <khsuranov@gmail.com>
2018-07-28 12:51:37 -0500

    hello

---
+++ src/sample/HelloWorld.java
@@ -0,0 +1,12 @@
+package sample;
+
+
+public class HelloWorld {
+	String str1 = "Hello ";
+	String str2 = "world!";
+	
+	public static void main(String[] args) {
+		HelloWorld hw = new HelloWorld();
+		System.out.println(hw.str1+hw.str2);
+	}
+}
